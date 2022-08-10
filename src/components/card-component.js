70a7127fcb268a6d20a00c6d6abeb35590964c65
oscar anillo <oscarani14@gmail.com>
2022-08-09 19:23:58 -0500

    Commit
    create card component and image component
    add card to App.js
    add styles for card component

---
+++ src/components/card-component.js
@@ -0,0 +1,10 @@
+import ImageComponent from "./image-component";
+
+export default function CardComponent(){
+    return (
+        <div className="card">
+            <ImageComponent />
+            {/* Components */}
+        </div>
+    )
+}
